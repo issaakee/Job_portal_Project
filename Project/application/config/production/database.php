f41165cf09a2c6e5edd1bc65a1196e19bab2651d
Ishak Hafdallah <95688861+issaakee@users.noreply.github.com>
2024-07-10 22:46:41 +0200

    Add files via upload

---
+++ Project/application/config/production/database.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+// Examples: mysql
+$active_group = 'mysql';
+$active_record = TRUE;
+
+//MySQL example
+$db['mysql']['hostname'] = 'localhost';
+$db['mysql']['username'] = 'mamp';
+$db['mysql']['password'] = 'ledwatch';
+$db['mysql']['database'] = 'PFE';
+$db['mysql']['dbdriver'] = 'mysqli';
+$db['mysql']['dbprefix'] = '';
+$db['mysql']['pconnect'] = FALSE;
+$db['mysql']['db_debug'] = FALSE;
+$db['mysql']['cache_on'] = FALSE;
+$db['mysql']['cachedir'] = '';
+$db['mysql']['char_set'] = 'utf8';
+$db['mysql']['dbcollat'] = 'utf8_general_ci';
+$db['mysql']['swap_pre'] = '';
+$db['mysql']['autoinit'] = TRUE;
+$db['mysql']['stricton'] = FALSE;
+
+$db['mysql']['port'] = 8889;
+
+/* End of file database.php */
+/* Location: ./application/config/database.php */
