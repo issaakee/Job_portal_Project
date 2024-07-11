6132577f1b8fde037c5d00a52a36e21e2a8d4040
Ishak Hafdallah <95688861+issaakee@users.noreply.github.com>
2024-07-11 10:09:15 +0200

    Add files via upload

---
+++ Project/application/views/admin/settings/template.php
@@ -0,0 +1,101 @@
+
+    <!-- Page heading -->
+    <div class="page-head">
+    <!-- Page heading -->
+        <h2 class="pull-left"><?php echo lang('Settings')?>
+		  <!-- page meta -->
+		  <span class="page-meta"><?php echo lang('System settings')?></span>
+		</h2>
+
+
+		<!-- Breadcrumb -->
+		<div class="bread-crumb pull-right">
+          <a href="<?php echo site_url('admin')?>"><i class="icon-home"></i> <?php echo lang('Home')?></a> 
+          <!-- Divider -->
+          <span class="divider">/</span> 
+          <a class="bread" href="<?php echo site_url('admin/settings')?>"><?php echo lang('Settings')?></a>
+          <!-- Divider -->
+          <span class="divider">/</span> 
+          <a class="bread-current" href="<?php echo site_url('admin/settings/template')?>"><?php echo lang('Template')?></a>
+		</div>
+
+		<div class="clearfix"></div>
+
+    </div>
+    <!-- Page heading ends -->
+
+
+
+    <!-- Matter -->
+
+    <div class="matter-settings">
+    
+    <div style="margin-bottom: 8px;" class="tabbable">
+      <ul class="nav nav-tabs settings-tabs">
+        <li><a href="<?php echo site_url('admin/settings/contact')?>"><?php echo lang('Company contact')?></a></li>
+        <li><a href="<?php echo site_url('admin/settings/language')?>"><?php echo lang('Languages')?></a></li>
+        <li class="active"><a href="<?php echo site_url('admin/settings/template')?>"><?php echo lang('Template')?></a></li>
+        <?php if(config_db_item('slug_enabled') === TRUE): ?>
+        <li><a href="<?php echo site_url('admin/settings/slug')?>"><?php echo lang_check('SEO slugs')?></a></li>
+        <?php endif; ?>
+        <?php if(config_db_item('currency_conversions_enabled') === TRUE): ?>
+        <?php endif; ?>
+      </ul>
+    </div>
+    
+    <div class="container">
+          <div class="row">
+
+            <div class="col-md-12">
+
+
+              <div class="widget wlightblue">
+                
+                <div class="widget-head">
+                  <div class="pull-left"><?php echo lang('Template settings')?></div>
+                  <div class="widget-icons pull-right">
+                    <a class="wminimize" href="#"><i class="icon-chevron-up"></i></a> 
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+
+                <div class="widget-content">
+                  <div class="padd">
+                    <?php echo validation_errors()?>   
+                    <?php if($this->session->flashdata('error')):?>
+                    <p class="label label-important validation"><?php echo $this->session->flashdata('error')?></p>
+                    <?php endif;?>            
+                    <hr />
+                    <!-- Form starts.  -->
+                    <?php echo form_open(NULL, array('class' => 'form-horizontal', 'role'=>'form'))?>                              
+
+                                
+                                <?php upload_field_admin('website_admin_logo', 'Website Admin logo', 2, $settings['website_admin_logo']); ?>
+                                <?php upload_field_admin('website_admin_favicon', 'Website Admin Favicon', 2, _ch($settings['website_admin_favicon'], '')); ?>
+
+                                <?php upload_field_admin('website_logo', 'Website logo', 2, $settings['website_logo']); ?>
+                                
+                                <?php if(config_item('secondary_logo_support') == TRUE):?> 
+                                <?php upload_field_admin('website_logo_secondary', 'Website logo secondary', 2, $settings['website_logo_secondary']); ?>
+                                <?php endif; ?>
+                    
+                                <?php upload_field_admin('website_favicon', 'Website Favicon', 2, $settings['website_favicon']); ?>
+                               
+                            
+                       <?php echo form_close()?>
+                  </div>
+                </div>
+                  <div class="widget-foot">
+                    <!-- Footer goes here -->
+                  </div>
+              </div>  
+
+            </div>
+          </div>
+    </div>
+    </div>
+
+	<!-- Matter ends -->
+
+   <!-- Mainbar ends -->	    	
+   <div class="clearfix"></div>
